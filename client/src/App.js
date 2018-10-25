ca91e48cc38f4129f86d46d04ccfe6727be594eb
elanonimo <mgrbmk@emailhosting.com>
2018-10-25 12:51:25 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {ApolloProvider} from 'react-apollo';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import HomeView from './views/HomeView';
+import client from './ApolloClient';
+import store from './store';
+import ApartmentView from "./views/ApartmentView";
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={HomeView} />
+              <Route path="/apartments/:apartmentId" component={ApartmentView} />
+            </Switch>
+          </Router>
+        </Provider>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/views/ApartmentAmentityView.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ApartmentAmentityView extends React.Component {
+  render() {
+    let {apartment, limit = 3} = this.props;
+    let amentities = [];
+    
+    /* apartment.amenities.map((item, index) => {
+      if (index < limit) {
+        return amentities.push(<span className="_1h9l4w0vvX6d56ZnJ3NLod" key={index}><i></i><span>{item}</span></span>);
+      } else {
+        return null;
+      }
+    }); */
+
+    apartment.amenities.forEach((item, index) => {
+      if (index < limit) {
+        amentities.push(<span className="_1h9l4w0vvX6d56ZnJ3NLod" key={index}><i></i><span>{item}</span></span>);
+      }
+    });
+    
+    return amentities;
+  }
+}
