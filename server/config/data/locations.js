670de15ebae2c78e9dc9688703e2d162f1dd5358
Oleh Biriukov <ol.biriukov@gmail.com>
2018-10-31 20:18:23 +0200

    fix wrong id's inside of db to make locations nested documents search

--- server/config/data/locations.js
+++ server/config/data/locations.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { _id: "5b62B540BcFF044ffA169f60", active: true, title: "Berlin", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
-  { _id: "507F191e810c19729De860eA", active: true, title: "Cologne", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
-  { _id: "53DC98133bAb79000034E25a", active: true, title: "Stuttgart", country: "4X2Lug8JgTq6zNMWnZzwDN5" }
+  { _id: "43B5wTxKUwTjTnW4hyZMcvZ", active: true, title: "Berlin", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
+  { _id: "44C5wTxKUwTjTnW4hyZMcvZ", active: true, title: "Cologne", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
+  { _id: "45E5wTxKUwTjTnW4hyZMcvZ", active: true, title: "Stuttgart", country: "4X2Lug8JgTq6zNMWnZzwDN5" }
 ];
